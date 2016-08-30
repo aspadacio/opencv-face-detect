6125516a2b6782ebf9dea0814bdeba657d8aa507
Sousa, A <amaurispadacio@gmail.com>
2016-08-30 14:56:52 -0300

    creating files and headers

---
+++ src/FaceDetectionVideo.cpp
@@ -0,0 +1,18 @@
+/*
+ * FaceDetectionVideo.cpp
+ *
+ *  Created on: 30 de ago de 2016
+ *      Author: xbtse
+ */
+
+#include "FaceDetectionVideo.h"
+
+FaceDetectionVideo::FaceDetectionVideo() {
+	// TODO Auto-generated constructor stub
+
+}
+
+FaceDetectionVideo::~FaceDetectionVideo() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ src/FaceDetectionVideo.h
@@ -0,0 +1,17 @@
+/*
+ * FaceDetectionVideo.h
+ *
+ *  Created on: 30 de ago de 2016
+ *      Author: xbtse
+ */
+
+#ifndef FACEDETECTIONVIDEO_H_
+#define FACEDETECTIONVIDEO_H_
+
+class FaceDetectionVideo {
+public:
+	FaceDetectionVideo();
+	virtual ~FaceDetectionVideo();
+};
+
+#endif /* FACEDETECTIONVIDEO_H_ */
